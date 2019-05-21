96069e396b2c3d90b2eff323940624be30b5aa25
dshmatkov <dshmatkov@yandex-team.ru>
2019-05-21 14:45:36 +0300

    upd

--- model/model.py
+++ model/model.py
@@ -203,7 +203,7 @@ def model_fn(features, labels, mode):
         if mode == tf.estimator.ModeKeys.TRAIN:
             with tf.variable_scope('optimizer'):
                 optimizer = tf.train.AdamOptimizer(
-                    learning_rate=0.001,
+                    learning_rate=0.1,
                     epsilon=0.1,
                 )
                 optimizer = tf.contrib.estimator.clip_gradients_by_norm(optimizer, 1)
