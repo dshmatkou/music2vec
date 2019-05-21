d0595347915e8c6abb97efeb962d12c45d6c8cc4
dshmatkov <dshmatkov@yandex-team.ru>
2019-05-22 02:57:01 +0300

    del sigmoid

--- model/model.py
+++ model/model.py
@@ -46,7 +46,6 @@ def build_kernel_model(input):
 
         result = tf.layers.dense(
             all_features, 200,
-            activation=tf.nn.sigmoid,
             kernel_initializer=tf.contrib.layers.xavier_initializer(seed=123)
         )
 
