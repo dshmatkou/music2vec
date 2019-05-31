6964d9c6d3a3c6385ea9517d5c7c89657afc98c7
dshmatkov <dshmatkov@yandex-team.ru>
2019-05-31 04:35:55 +0300

    push demo cli

--- model/demo.py
+++ model/demo.py
@@ -1,4 +1,5 @@
 import csv
+import numpy as np
 import tensorflow as tf
 from model.model import model_fn
 from preprocess_dataset.audio.processors import get_processor
@@ -7,7 +8,7 @@ PROCESSOR = 'mfcc'
 
 
 def load_genres(genres_metadata_fn):
-    with open(genres_metadata_fn, 'rb') as f:
+    with open(genres_metadata_fn, 'r') as f:
         reader = csv.DictReader(f)
         items = [
             (int(item['genre_id']), item['title'])
@@ -29,6 +30,7 @@ def demonstrate(music_file, model_path, genres_metadata_fn):
     processor = get_processor(PROCESSOR)
     genres = load_genres(genres_metadata_fn)
     features = processor(music_file)
+    features = np.reshape(features, (1,) + features.shape + (1,))
 
     estimator = tf.estimator.Estimator(
         model_fn=model_fn,
@@ -40,11 +42,12 @@ def demonstrate(music_file, model_path, genres_metadata_fn):
 
     predictions = estimator.predict(
         input_fn=tf.estimator.inputs.numpy_input_fn(
-            x=[features],
+            x={'feature': features},
             shuffle=False
         )
     )
 
-    result = extract_genres(predictions[0]['genres_top'], genres)
+    pred = next(predictions)
+    result = extract_genres(pred['genres_top'], genres)
     for genre_title, prob in result:
         print(genre_title, ':', prob)
