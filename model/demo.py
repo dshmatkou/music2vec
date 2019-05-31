4ff55f942871c4921aeb7dec84966179025fc6a3
dshmatkov <dshmatkov@yandex-team.ru>
2019-05-31 11:13:36 +0300

    upd

--- model/demo.py
+++ model/demo.py
@@ -3,6 +3,7 @@ import numpy as np
 import tensorflow as tf
 from model.model import model_fn
 from preprocess_dataset.audio.processors import get_processor
+from tensorflow.contrib import predictor
 
 PROCESSOR = 'mfcc'
 
@@ -32,22 +33,23 @@ def demonstrate(music_file, model_path, genres_metadata_fn):
     features = processor(music_file)
     features = np.reshape(features, (1,) + features.shape + (1,))
 
-    estimator = tf.estimator.Estimator(
-        model_fn=model_fn,
-        config=tf.estimator.RunConfig(
-            save_checkpoints_steps=10,
-            model_dir=model_path,
-        )
-    )
+    predict_fn = predictor.from_saved_model(model_path)
+    # estimator = tf.estimator.Estimator(
+    #     model_fn=model_fn,
+    #     config=tf.estimator.RunConfig(
+    #         save_checkpoints_steps=10,
+    #         model_dir=model_path,
+    #     )
+    # )
+    #
+    # predictions = estimator.predict(
+    #     input_fn=tf.estimator.inputs.numpy_input_fn(
+    #         x={'feature': features},
+    #         shuffle=False
+    #     )
+    # )
 
-    predictions = estimator.predict(
-        input_fn=tf.estimator.inputs.numpy_input_fn(
-            x={'feature': features},
-            shuffle=False
-        )
-    )
-
-    pred = next(predictions)
-    result = extract_genres(pred['genres_top'], genres)
+    predictions = predict_fn({'feature': features})
+    result = extract_genres(predictions['genres_top'], genres)
     for genre_title, prob in result:
         print(genre_title, ':', prob)
