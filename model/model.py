9a2068846f5915d54987d9dab78155057fcf95d5
dshmatkov <dshmatkov@yandex-team.ru>
2019-05-31 04:04:01 +0300

    Remove activation from logit loss

--- model/model.py
+++ model/model.py
@@ -143,7 +143,6 @@ def build_simple_logit_loss(kernel_model, label, label_name):
             inputs=kernel_model,
             units=units,
             kernel_initializer=tf.contrib.layers.xavier_initializer(seed=123),
-            activation=tf.nn.sigmoid,
         )
         summaries.append(tf.summary.tensor_summary('prediction', pred))
 
